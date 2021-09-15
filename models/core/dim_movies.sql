edb608514168bd9b779dd6c962966e5c24db03c3
victoria-tier <73898175+victoria-tier@users.noreply.github.com>
2021-09-15 22:04:38 +0200

    ads dim models

---
+++ models/core/dim_movies.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='table') }}
+
+select   
+  show_id, 
+  category,
+  title, 
+  director,
+  principal_cast, 
+  country, 
+  release_year,
+  rating,
+  duration, 
+  genres
+from {{ref('stg_netflix_titles')}}
+where category = 'Movie'
