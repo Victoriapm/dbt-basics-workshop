1ed2e2da9b3adf829150a012ca2ecf236dd0de99
victoria-tier <73898175+victoria-tier@users.noreply.github.com>
2021-09-15 22:04:49 +0200

    ads field definition

--- models/staging/stg_netflix_titles.sql
+++ models/staging/stg_netflix_titles.sql
@@ -2,7 +2,16 @@
 
 
 select
-    *
+  show_id::varchar(16) as show_id, 
+  "TYPE"::varchar(16) as category,
+  title::varchar(64) as title, 
+  director::varchar(64) as director,
+  "CAST"::varchar(1024) as principal_cast, 
+  country::varchar(64) as country, 
+  release_year::varchar(4) as release_year,
+  rating::varchar(16) as rating,
+  duration::varchar(16) as duration, 
+  listed_in::varchar(64) as genres
 from {{ ref('netflix_titles') }}
 
 -- dbt run --model <model.sql> --var 'is_test_run: false'
