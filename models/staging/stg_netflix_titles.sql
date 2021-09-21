428abe74334d89cb6482309ea3df51847c290c6d
Victoria Perez Mola <4315804+Victoriapm@users.noreply.github.com>
2021-09-21 21:54:12 +0200

    adds seasons

--- models/staging/stg_netflix_titles.sql
+++ models/staging/stg_netflix_titles.sql
@@ -3,15 +3,20 @@
 
 select
   show_id::varchar(16) as show_id, 
-  "TYPE"::varchar(16) as category,
-  title::varchar(64) as title, 
+  type::varchar(16) as category,
+  title::varchar(128) as title, 
   director::varchar(64) as director,
-  "CAST"::varchar(1024) as principal_cast, 
-  country::varchar(64) as country, 
+  principal_cast::varchar(1024) as principal_cast, 
+  country::varchar(128) as country, 
   release_year::varchar(4) as release_year,
   rating::varchar(16) as rating,
-  duration::varchar(16) as duration, 
-  listed_in::varchar(64) as genres
+  case when duration like '%Seasons' then 
+    replace(duration,'Seasons','')::int 
+    else null end as seasons,
+  case when duration like '%min' then 
+    replace(duration,'min','')::int 
+    else 0 end as duration,
+  listed_in::varchar(128) as genres
 from {{ ref('netflix_titles') }}
 
 -- dbt run --model <model.sql> --var 'is_test_run: false'
