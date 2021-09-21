57d1d05aa73bf954f484693d0bdbdbef16e063e6
victoria-tier <73898175+victoria-tier@users.noreply.github.com>
2021-09-21 22:00:15 +0200

    adds dev limit

--- models/core/dim_movies.sql
+++ models/core/dim_movies.sql
@@ -13,3 +13,9 @@ select
   genres
 from {{ref('stg_netflix_titles')}}
 where category = 'Movie'
+-- dbt run --model <model.sql> --var 'is_test_run:false'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
